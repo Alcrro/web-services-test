2a5853667f2d8b4bebf713450b1cd1d4e98aa216
alcrro <alex.roventa94@gmail.com>
2025-12-06 23:28:07 +0200

    new nexts version

--- src/proxy.ts
+++ src/proxy.ts
@@ -1,7 +1,7 @@
 import { NextRequest, NextResponse } from "next/server";
 import { getSession } from "./shared/utils/getSession";
 
-export async function middleware(req: NextRequest) {
+export async function proxy(req: NextRequest) {
   try {
     const token = await getSession();
 
