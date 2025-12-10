8a4c690f09b403d97e95f05a2ddd65b39c461c3f
alcrro <alex.roventa94@gmail.com>
2025-12-10 16:50:05 +0200

    move portofolio projects in server side with serrch params v2.1

--- src/components/organisms/PortfolioContent.tsx
+++ src/components/organisms/PortfolioContent.tsx
@@ -1,7 +1,6 @@
-import { FC, Suspense } from "react";
+import { FC } from "react";
 import { PortfolioPageContent } from "@/shared/data/consts/portfololioPage/portfolioPageContent";
 import HeroSection from "./portfolio/HeroSection";
-import PortfolioProjectsClient from "../molecules/portfolio/PortfolioProjectsClient";
 import TestimonialsSection from "../molecules/Cards/testimonials/TestimonialSection";
 import PortfolioCta from "../molecules/portfolio/PortfolioCta";
 
@@ -10,15 +9,14 @@ interface PortfolioContentProps {
 }
 
 const PortfolioContent: FC<PortfolioContentProps> = ({ portfolioContent }) => {
-  const { hero, projects, filters, testimonials, cta } = portfolioContent;
+  const { hero, testimonials, cta } = portfolioContent;
 
   return (
     <main className="space-y-32 px-6 md:px-12 py-16 max-w-7xl mx-auto">
       <HeroSection {...hero} />
 
-      <Suspense fallback={"Loading projects..."}>
-        <PortfolioProjectsClient projects={projects} filters={filters} />
-      </Suspense>
+      {/* <PortfolioProjectsClient projects={projects} filters={filters} />
+       */}
 
       {testimonials && <TestimonialsSection {...testimonials} />}
 
