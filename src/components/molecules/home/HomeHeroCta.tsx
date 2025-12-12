8e384d31668b045311b014fef6541560c0d96715
alcrro <alex.roventa94@gmail.com>
2025-12-12 16:19:37 +0200

    feature issue #11 homeHeroCta component

---
+++ src/components/molecules/home/HomeHeroCta.tsx
@@ -0,0 +1,25 @@
+import Button from "@/shared/ui/Button";
+import { homePageContent } from "@/shared/data/consts/homePage/homePageContent";
+
+const HomeHeroCta = () => {
+  return (
+    <>
+      <Button
+        variant="primary"
+        href={homePageContent.hero.cta.primary.link}
+        className="w-full max-w-60 animate-scaleUp "
+      >
+        {homePageContent.hero.cta.primary.text}
+      </Button>
+      <Button
+        variant="primary"
+        href={homePageContent.hero.cta.secondary.link}
+        className="w-full max-w-60 animate-scaleUp "
+      >
+        {homePageContent.hero.cta.secondary.text}
+      </Button>
+    </>
+  );
+};
+
+export default HomeHeroCta;
