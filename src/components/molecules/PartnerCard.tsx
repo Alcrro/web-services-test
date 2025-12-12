b8cf34aa181a830090d7f930c2398dbc706ebbf3
alcrro <alex.roventa94@gmail.com>
2025-12-12 15:38:56 +0200

    feature #14 partner card component implementation

---
+++ src/components/molecules/PartnerCard.tsx
@@ -0,0 +1,29 @@
+import { IHomePartners } from "@/shared/data/consts/homePage/homePageContent";
+import { FC } from "react";
+import style from "@/components/styles/partnerList.module.scss";
+import Image from "next/image";
+import { linkString } from "@/shared/utils/generatePartnerMarqueeLink";
+
+interface IHomePartnersProps {
+  partner: IHomePartners;
+}
+const PartnerCard: FC<IHomePartnersProps> = ({ partner }) => {
+  return (
+    <div className={`${style.marquee_item}`}>
+      {!partner.logo && partner.link ? (
+        <div>{linkString(partner.link)}</div>
+      ) : (
+        <Image
+          src={partner.logo}
+          alt={partner.name}
+          width={160}
+          height={60}
+          className="object-contain"
+        />
+      )}
+      <div className="mt-2 text-center">{partner.name}</div>
+    </div>
+  );
+};
+
+export default PartnerCard;
