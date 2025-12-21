d6a9a750108f8d637d0c8fe974864466b6493334
Alexandru Roventa <96865252+Alcrro@users.noreply.github.com>
2025-12-21 02:33:24 +0200

    Refactor PostCSS config to use CommonJS format

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -1,5 +1,8 @@
-const config = {
-  plugins: ["@tailwindcss/postcss"],
+module.exports = {
+  plugins: {
+    '@tailwindcss/postcss': {},
+    autoprefixer: {},
+  },
 };
 
-export default config;
+
